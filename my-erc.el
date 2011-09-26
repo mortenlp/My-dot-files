478a655c9533b4116e95afb49e582b0a7e97d5d6
Morten Leander Petersen <mortenlp2@gmail.com>
2011-09-26 19:01:09 +0200

    Added my-erc.el

---
+++ my-erc.el
@@ -0,0 +1,9 @@
+(require 'erc)
+
+;; check channels
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+
+                                "324" "329" "332" "333" "353" "477"))
+;; don't show any of this
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
