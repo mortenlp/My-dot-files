22990daa6ac3e6d809fce3533bb4ed3ec82f1326
Morten Leander Petersen <mortenlp2@gmail.com>
2011-10-05 21:51:53 +0200

    Deleted elpa again.

--- my-dot-emacs.el
+++ my-dot-emacs.el
@@ -93,14 +93,3 @@
 ;; Don't disable upcase/downcase functions
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
-
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
