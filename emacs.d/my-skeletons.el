c04a12c61704b1f400368f9b6798d2f229fd22f1
Morten Leander Petersen <mortenlp@mortenlp-ThinkPad-SL510.(none)>
2012-05-29 22:52:55 +0200

    Added skeleton for presentations using beamer.

--- emacs.d/my-skeletons.el
+++ emacs.d/my-skeletons.el
@@ -40,3 +40,31 @@
   \n
   "}" > \n
   )
+
+(define-skeleton beamer-skeleton
+  "\n"
+  "\n"
+  "#+TITLE:     Title\n"
+  "#+author:    Morten Leander Petersen\n"
+  "#+EMAIL:     mortenlp2@gmail.com\n"
+  "#+DATE:      2012-05-23 ons\n"
+  "#+DESCRIPTION:\n"
+  "#+KEYWORDS:\n"
+  "#+LANGUAGE:  en\n"
+  "#+OPTIONS:   H:3 num:t toc:t \\n:nil @:t ::t |:t ^:t -:t f:t *:t <:t\n"
+  "#+OPTIONS:   TeX:t LaTeX:t skip:nil d:nil todo:t pri:nil tags:not-in-toc\n"
+  "#+INFOJS_OPT: view:nil toc:nil ltoc:t mouse:underline buttons:0 path:http://orgmode.org/org-info.js\n"
+  "#+EXPORT_SELECT_TAGS: export\n"
+  "#+EXPORT_EXCLUDE_TAGS: noexport\n"
+  "#+LINK_UP:\n"
+  "#+LINK_HOME:\n"
+  "#+XSLT:\n"
+  "#+startup: beamer\n"
+  "#+LaTeX_CLASS: beamer\n"
+  "#+LaTeX_CLASS_OPTIONS: [bigger]\n"
+  "#+BEAMER_FRAME_LEVEL: 2\n"
+  "#+COLUMNS: %40ITEM %10BEAMER_env(Env) %9BEAMER_envargs(Env Args) %4BEAMER_col(Col) %10BEAMER_extra(Extra)\n"
+  "#+latex_header: \\mode<beamer>{\\usetheme{Madrid}}\n"
+  "#+latex_header: \\mode<beamer>{\\usetheme{Copenhagen}}\n"
+  "#+latex_header: \\mode<beamer>{\\useoutertheme[subsection=false]{smoothbars}}\n"
+  )
