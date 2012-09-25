7e001cc9a873437d8ccd568f3fb1c0ef7a6b16bf
Morten Leander Petersen <mortenlp@mortenlp-ThinkPad-SL510.(none)>
2012-09-25 16:49:43 +0200

    Updated settings for multiple-cursors

--- emacs.d/my-bindings.el
+++ emacs.d/my-bindings.el
@@ -108,7 +108,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/add-multiple-cursors-to-region-lines)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 
